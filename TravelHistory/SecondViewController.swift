61fa7426c05e6db9168f934189782994613e8a78
Burak Yanmaz <burakyanmaz@me.com>
2019-05-06 22:07:40 +0300

    First commit

---
+++ TravelHistory/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TravelHistory
+//
+//  Created by Burak Yanmaz on 6.05.2019.
+//  Copyright © 2019 Simpler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
